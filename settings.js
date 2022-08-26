73e63ef80aaaf8bc7a973ec4cbd4e329717b3158
RennStore123 <110625568+RennStore123@users.noreply.github.com>
2022-08-26 20:27:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,38 +1,74 @@
 const fs = require('fs')
 const chalk = require('chalk')
+const  { indonesia, english, spanyol} = require(`./language`)
 
-// Free Apikey
+// Website Api
 global.APIs = {
-	riy: 'https://api-xriy04.herokuapp.com',
+	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan ke owner
 }
+
+// Free apikey
+global.APIKeys = {
+	'https://api.zeeoneofc.xyz': 'V12',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+}
+
+// setting 
+global.autoread = false // auto read pesan / message
+global.autobio = true 
+
+//language
+//Available in indonesia , english & spanyol 
+//You can request others language 
+global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
+
 
 // Other
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.youtube = 'https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g'
-global.ownername = 'YahyaGanzz'
-global.botname ='AzzBot-Md'
-global.donasi = {
-    saweria: 'Kaga Punya',
-    nomor: '081511480762'
-}
-global.packname = 'AzzBot'
-global.author = 'WhatsApp Bot'
+// Other
+global.botname = "Renn-md" //namabot kalian
+global.ownername= "Renn-md" //nama kalian
+global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.github = "https://zeeone-ofc.github.io/" //bebas
+global.email = "derentino99@gmail.com" //bebas
+global.region = "Indonesia" //bebas
+global.timezone = 'Asia/Jakarta' //  timezone wib
+global.premium = ['6282184976717'] //premium user
+global.owner = ["6282184976717","6282184976717"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6282184976717" // nomor wa kalian
+global.ownernomerr = "+6282184976717" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.packname = 'Renn-md' //sticker wm ubah
+global.author = 'Di Buat Oleh Renn-md' //sticker wm ganti nama kalian
 global.sessionName = 'session'
+global.typemenu = 'document'
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 9999
+}
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: '✓ Success',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Loading...'
+    sukses: '🤗Done, Oke Desu~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
-global.thumb = fs.readFileSync('./image/hinata.jpg')
-global.vn = './sound/menu.mp3'
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
